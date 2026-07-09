76408ef3b77bfb69cd280e8640aea9e16203ec13
Peter_Pei <mmiaomiao140@gmail.com>
2026-07-09 22:54:54 +0800

    Add files via upload

---
+++ leetGPU/Interleave Arrays.py
@@ -0,0 +1,23 @@
+import torch
+import triton
+import triton.language as tl
+
+
+@triton.jit
+def interleave_kernel(A_ptr, B_ptr, output_ptr, N, BLOCK_SIZE: tl.constexpr):
+    pid = tl.program_id(axis=0)
+    offset = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    mask = offset < N
+    a = tl.load(A_ptr + offset, mask=mask)
+    b = tl.load(B_ptr + offset, mask=mask)
+    tl.store(output_ptr + offset * 2, a, mask=mask)
+    tl.store(output_ptr + offset * 2 + 1, b, mask=mask)
+
+# A, B, output are tensors on the GPU
+def solve(A: torch.Tensor, B: torch.Tensor, output: torch.Tensor, N: int):
+    BLOCK_SIZE = 256
+
+    def grid(meta):
+        return (triton.cdiv(N, meta["BLOCK_SIZE"]),)
+
+    interleave_kernel[grid](A, B, output, N, BLOCK_SIZE=BLOCK_SIZE)
